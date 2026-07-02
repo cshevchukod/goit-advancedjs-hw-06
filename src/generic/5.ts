1757c6e76610c929e82e5447c581111dc6948749
Kostiantyn Shevchuk <constantine.shevchuk@gmail.com>
2026-07-03 02:53:25 +0300

    final1

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,19 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<TKey, TValue> {
+  key: TKey;
+  value: TValue;
 }
+
+const pair1: KeyValuePair<string, number> = {
+  key: "age",
+  value: 30,
+};
+
+const pair2: KeyValuePair<number, string> = {
+  key: 1,
+  value: "first",
+};
 
 export {};
